014185a8a4969d48641189a6e80acc007a2ea245
Faldiii15 <rifaldif888@gmail.com>
2025-05-09 10:05:40 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fasum/firebase_options.dart';
 import 'package:fasum/screens/home_screen.dart';
-import 'package:fasum/';
-import 'package:fasum_app/screens/sign_in_screen.dart';
+import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:';
 import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async {
